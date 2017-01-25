6d4d73a5a8ec0bb43bd32475c7c2dd24346935d6
Maria Bajena <mbajena@vp.pl>
2017-01-25 11:29:59 +0100

    8.4

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var a=3,
+	h=4,
+	triangleArea=1;
+
+triangleArea=a*h/2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+
+console.log(triangleArea);
+
