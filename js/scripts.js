60a54809b48a003b0dbd734aea41ff5fcfb07fa9
Maria Bajena <mbajena@vp.pl>
2017-01-25 13:17:53 +0100

    zad.8.6

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
 var a=1,
-	b=1,
-	value=1;
+	h=2;
 
-value=(a*a) + (2 * a * b) - (b*b);
+function getTriangleArea(a, h) {
+	if (a<=0 || h<=0) {
+		console.log('Nieprawidłowe dane');
+	}
 
-console.log(value);
-
-if (value<0) {
-	console.log('wynik ujemny')
+	else if (a>0 && h>0) {
+		return a*h/2;
+	}
 }
-	else if (value>0) {
-		console.log('wynik dodatni')
-	}
-	else if (value=0) {
-		console.log('wynik=0')
-	}
+
+console.log( getTriangleArea(10,6) );
+
+var triangle1Area = getTriangleArea(10, 6),
+	triangle2Area = getTriangleArea(10, 5),
+	triangle3Area = getTriangleArea(10, 4);
+
+console.log( triangle1Area + " , " + triangle2Area + " , " + triangle3Area);
