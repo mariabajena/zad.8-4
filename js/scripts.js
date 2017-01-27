937c47dfbe3bef2eb0192f1426aa0989eea69429
Maria Bajena <mbajena@vp.pl>
2017-01-27 15:32:33 +0100

    zad.8.10

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
-function rysujChoinke(h){
-	for (var i=1; i<=h; i++){
-		var star="";
-		var n=i;
-		for (var j=1; j<=n*2-1; j++){
-			star+="*";
-		}
-		console.log(star);
-	}
-}
+var withButtonClass = document.getElementsByClassName('button');
 
-rysujChoinke(3);
+for (var i=0; i<withButtonClass.length; i++) {
+	alert(withButtonClass[i].innerText);
+}
